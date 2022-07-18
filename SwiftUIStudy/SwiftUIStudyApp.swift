477b96c3b0a9e72489637cb4019d090c61086368
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-18 09:28:36 +0300

    Initial Commit

---
+++ SwiftUIStudy/SwiftUIStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStudyApp.swift
+//  SwiftUIStudy
+//
+//  Created by Ömer Faruk Kılıçaslan on 18.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
